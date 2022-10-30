eb14c2ba23ff081315791877b6273989e7961484
Son <70love07@naver.com>
2022-10-30 10:28:00 +0900

    -연산자 프로그램에서 데이터를 처리하여 결과를 산출하는 것을 연산이라고 한다. 연산의 방향과 우선순위를 배우고 단항 연산자와 이항연산자, 삼항 연산자를 배웠다.

--- src/variable/Promotion.java
+++ src/variable/Promotion.java
@@ -1,2 +1,23 @@
-package variable;public class Promotion {
+package variable;
+
+public class Promotion {
+    public static void main(String[] args) {
+        // byte(1) < short(2) < int(4) < long(8) < float(4) < double(8)
+
+        byte ByteValue = 10;
+        int intValue = ByteValue; // int <- byte
+        System.out.println(intValue);
+
+        char charValue = '가';
+        intValue = charValue; // int <- char
+        System.out.println("가의 유니코드 = " + intValue);
+
+        intValue = 500;
+        long longValue = intValue; // long <- int
+        System.out.println(longValue);
+
+        intValue = 200;
+        double doubleValue = intValue; // double <- int
+        System.out.println(doubleValue);
+    }
 }
